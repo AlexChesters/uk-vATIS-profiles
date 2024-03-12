146c728bfd9ec56dd71f80099e3cc1401aa64f7c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-03-12 11:18:12 +0000

    add schema tests

--- Makefile
+++ Makefile
@@ -11,5 +11,6 @@ run: clean
 package: run
 	cd dist && zip -9 -r ../uk-vatis-profiles.zip . && cd ..
 
-test: clean
+test: run
+	poetry run pytest -s .
 	poetry run pylint uk_vatis_profiles
---
+++ tests/test_gmc.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+import json
+
+from parameterized import parameterized
+from jsonschema import validate
+
+# absolute path to the directory name this script lives in
+SCRIPT_DIR = os.path.dirname(__file__)
+dist_dir_path = os.path.join(SCRIPT_DIR, ".." , "dist")
+schemas_dir = os.path.join(SCRIPT_DIR, "schemas")
+
+positions = os.listdir(dist_dir_path)
+
+class GMCSchemaTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @parameterized.expand(positions)
+    def test_schema_passes(self, position):
+        with open(os.path.join(schemas_dir, position), "r", encoding="utf-8") as f:
+            schema = json.load(f)
+
+        with open(os.path.join(dist_dir_path, position), "r", encoding="utf-8") as f:
+            profile = json.load(f)
+
+        validate(instance=profile, schema=schema)
